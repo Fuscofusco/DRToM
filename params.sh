2baf89661ddd7ca471002f25e1cfbe5c3ab666e4
fuscomus <fuscomus@thuner-gw34.cpp.ualberta.ca>
2026-02-28 15:26:26 -0700

    Update: pushing all local changes

--- params.sh
+++ params.sh
@@ -5,15 +5,15 @@
 # ----------------------------
 CM_ENERGIES=( 13.0 )    # CoM energies in TeV (13.0 13.6)
 
-NPARTONS_LIST=( 4 )          # Number of partons in the final state 
-DRS=( 2 3 4 5 6 7 8 9 10 11 )      # DR values in TeV
-# DRS=( 11 )                    
+NPARTONS_LIST=( 4 )          # Number of partons in the final state (2,3,4,5 allowed)
+# DRS=( 2 3 4 5 6 7 8 9 10 11 )      # DR values in TeV (any value allowed)
+DRS=( 7 8 )                    
 
 WINDOW_START=2               # Starting mass (TeV) for slice
 WINDOW_END=11                 # Ending mass (TeV) for slice
-WINDOW_STEP=1                # Step size (TeV) 
+WINDOW_STEP=0.1                # Step size (TeV) 
 
-WINDOW_EVENTS=50             # Number of events per slice 
+WINDOW_EVENTS=100000             # Number of events per slice 
 
 # ----------------------------
 # Build generation slices e.g. "2.00:3.00", "3.00:4.00", ...
